07dba3b30d323d0b80ec4e3a1ae195a5045fe3b7
nicky <nicolesanaipei@gmail.com>
2024-11-21 14:04:18 +0300

    Adding a new task

--- script.js
+++ script.js
@@ -4,4 +4,14 @@ document.querySelector('#push').onclick = function () {
     if (document.querySelector('#newtask input').value.length == 0) {
         alert("Please Enter The Task To Be Created")
     }
+    else{
+        document.querySelector("#tasks").innerHTML += 
+        `
+        <div class="task">
+            <span id="taskname">
+                ${document.querySelector("#newtask input").value}
+            </span>
+        </div>
+        `;
+    }
 }  
